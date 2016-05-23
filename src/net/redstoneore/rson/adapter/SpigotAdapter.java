a17c6f51154e2a0c03c583ce6bb8704bfff4447c
Mark Hughes <m@rkhugh.es>
2016-05-23 14:30:24 +1000

    Preparing spigot adapter

--- src/net/redstoneore/rson/adapter/SpigotAdapter.java
+++ src/net/redstoneore/rson/adapter/SpigotAdapter.java
@@ -1,11 +1,17 @@
 package net.redstoneore.rson.adapter;
 
+import com.google.gson.GsonBuilder;
+
 public class SpigotAdapter {
 	
 	private static SpigotAdapter i;
 	public static SpigotAdapter get() {
 		if (i == null) i = new SpigotAdapter();
 		return i;
+	}
+	
+	public void addAdaptersOn(GsonBuilder gsonBuilder) {
+
 	}
 	
 }
