805c20d9d675e89a337e6563a6614fed38f0e811
Mark Hughes <m@rkhugh.es>
2016-05-23 14:30:17 +1000

    Adding Sponge BlockType adapter

--- src/net/redstoneore/rson/adapter/SpongeAdapter.java
+++ src/net/redstoneore/rson/adapter/SpongeAdapter.java
@@ -1,5 +1,11 @@
 package net.redstoneore.rson.adapter;
 
+import org.spongepowered.api.block.BlockType;
+
+import com.google.gson.GsonBuilder;
+
+import net.redstoneore.rson.typeadapter.sponge.TypeAdapterBlockType;
+
 public class SpongeAdapter {
 
 	private static SpongeAdapter i;
@@ -7,5 +13,9 @@ public class SpongeAdapter {
 		if (i == null) i = new SpongeAdapter();
 		return i;
 	}
-
+	
+	public void addAdaptersOn(GsonBuilder gsonBuilder) {
+		gsonBuilder.registerTypeAdapter(BlockType.class, new TypeAdapterBlockType());
+	}
+	
 }
---
+++ src/net/redstoneore/rson/typeadapter/sponge/TypeAdapterBlockType.java
@@ -0,0 +1,28 @@
+package net.redstoneore.rson.typeadapter.sponge;
+
+import java.lang.reflect.Type;
+
+import org.spongepowered.api.block.BlockType;
+import org.spongepowered.common.registry.type.BlockTypeRegistryModule;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSerializationContext;
+
+import net.redstoneore.rson.typeadapter.TypeAdapter;
+
+public class TypeAdapterBlockType extends TypeAdapter<BlockType> {
+	
+	@Override
+	public JsonElement toJsonElement(BlockType src, Type typeOfSrc, JsonSerializationContext context) {
+		return this.createPrimitive(src.getName());
+	}
+
+	@Override
+	public BlockType valueOf(JsonElement value) {
+		for (BlockType type : BlockTypeRegistryModule.getInstance().getAll()) {
+			if (type.getName() == value.getAsString()) return type;
+		}
+		return null;
+	}
+	
+}
